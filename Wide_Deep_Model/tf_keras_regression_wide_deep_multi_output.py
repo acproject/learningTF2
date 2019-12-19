bea7fe938ddca15cbb621558981f1c8888594afe
qinhangyu <acproject@qq.com>
2019-12-19 10:32:51 +0800

    增加多输出和多输入的模型

--- Wide_Deep_Model/tf_keras_regression_wide_deep_multi_output.py
+++ Wide_Deep_Model/tf_keras_regression_wide_deep_multi_output.py
@@ -57,9 +57,10 @@ input_wide = tf.keras.layers.Input(shape=[5])
 input_deep = tf.keras.layers.Input(shape=[6])
 hidden1 = tf.keras.layers.Dense(30, activation="relu")(input_deep)
 hidden2 = tf.keras.layers.Dense(30, activation='relu')(hidden1)
-concat = tf.keras.layers.concatenate([input_wide,hidden2])
+concat = tf.keras.layers.concatenate([input_wide, hidden2])
 output = tf.keras.layers.Dense(1)(concat)
-model = tf.keras.models.Model(inputs= [input_wide, input_deep], outputs=[output])
+output2 = tf.keras.layers.Dense(1)(hidden2)
+model = tf.keras.models.Model(inputs=[input_wide, input_deep], outputs=[output, output2])
 model.build(input_shape=(None,8))
 
 model.summary()
@@ -77,8 +78,8 @@ x_valid_scaled_deep = x_valid_scaled[:, 2:]
 x_test_scaled_wide = x_test_scaled[:, :5]
 x_test_scaled_deep = x_test_scaled[:, 2:]
 history = model.fit(
-    [x_train_scaled_wide, x_train_scaled_deep], y_train,
-    validation_data=([x_valid_scaled_wide, x_valid_scaled_deep], y_valid), epochs=100, callbacks=callbacks)
+    [x_train_scaled_wide, x_train_scaled_deep], [y_train, y_train],
+    validation_data=([x_valid_scaled_wide, x_valid_scaled_deep], [y_valid, y_valid]), epochs=100, callbacks=callbacks)
 
 def plot_learning_curves(history):
     pd.DataFrame(history.history).plot(figsize=(8, 5))
@@ -88,4 +89,4 @@ def plot_learning_curves(history):
 
 plot_learning_curves(history)
 
-model.evaluate([x_test_scaled_wide, x_test_scaled_deep], y_test)
+model.evaluate([x_test_scaled_wide, x_test_scaled_deep], [y_test, y_test])
